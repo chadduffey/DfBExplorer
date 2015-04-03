0fbb18a8fa4196a440b807ffdfb4662fbe3b1e1f
Chad Duffey <chadduffey@hotmail.com>
2015-04-03 11:29:54 +1100

    fixed small datetime issue

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment 
-import datetime
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -9,11 +9,11 @@ moment = Moment(app)
 
 @app.route('/')
 def index():
-	return render_template('index.html', current_time=datetime.datetime.utcnow())
+	return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-	return render_template('index.html', name=name, current_time=datetime.datetime.utcnow())
+	return render_template('index.html', name=name, current_time=datetime.utcnow())
 
 @app.errorhandler(404)
 def page_not_found(e):
