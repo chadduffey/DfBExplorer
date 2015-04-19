d0bdf6dcea9a069ed3098a6555757b44a4f5cc5b
Chad Duffey <chadduffey@hotmail.com>
2015-04-19 17:36:19 +1000

    mail commented out

--- routes.py
+++ routes.py
@@ -39,12 +39,13 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 #mail
-mail = Mail(app)
-app.config['MAIL_SERVER'] = 'email-smtp.us-west-2.amazonaws.com'
-app.config['MAIL_PORT'] = '587'
-app.config['MAIL_USE_TLS'] = True
+# mail = Mail(app)
+# app.config['MAIL_SERVER'] = 'email-smtp.us-west-2.amazonaws.com'
+# app.config['MAIL_PORT'] = '587'
+# app.config['MAIL_USE_TLS'] = True
 #app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 #app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
 #--------------------------------------------------------------
 
 def make_shell_context():
